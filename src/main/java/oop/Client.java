30aab3cb075ff47e7c9fb9019859b8939e3319df
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-09-28 11:40:20 -0400

    ADD >  exmaple oop

---
+++ src/main/java/oop/Client.java
@@ -0,0 +1,22 @@
+package oop;
+
+public class Client {
+
+    public static void main (String[]args){
+        Person personA = new Person();
+        personA.setName("Eynar");
+
+        Person personB = new Person();
+        personB.setName("Agustin");
+
+        Person personC = new Person();
+        personC.setName("Enzo");
+
+        System.out.println("person A: "+personA.getName());
+        System.out.println("person B: "+personB.getName());
+        System.out.println("person C: "+personC.getName());
+
+
+    }
+
+}
